d8071ca539bab7069498fb31937c5865f9408e53
Jiang Weiliang <jiang1984@gmail.com>
2020-12-01 02:06:16 +0800

    Create norepinephrine.sql

---
+++ norepinephrine.sql
@@ -0,0 +1,11 @@
+DROP MATERIALIZED VIEW IF EXISTS norepinephrine; 
+CREATE MATERIALIZED VIEW norepinephrine AS
+select
+  stay_id, linkorderid
+  , rate as vaso_rate
+  , amount as vaso_amount
+  , starttime
+  , endtime
+	,date_part('epoch'::text, endtime - starttime) / 60::double precision / 60::double precision AS duration_hours
+from inputevents
+where itemid = 221906 -- norepinephrine
